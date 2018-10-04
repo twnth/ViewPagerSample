274e1606a97556d438cb08b076cea88ca47820ee
lee <dudgns8704@naver.com>
2018-10-04 20:31:40 +0900

    btn data binding

--- app/src/main/java/com/example/dudgn/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dudgn/myapplication/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.dudgn.myapplication;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.dudgn.myapplication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.btn.setText("hello");
     }
 }
